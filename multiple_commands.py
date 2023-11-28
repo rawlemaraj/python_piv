6985675ce3bc6a4cc9a615f5d8aa96db2af47d4d
rawlemaraj <rawlemaraj@hotmail.com>
2023-11-28 10:00:40 -0400

    3rd

--- multiple_commands.py
+++ multiple_commands.py
@@ -10,6 +10,10 @@ def read_commands(file_path):
     df = pd.read_csv(file_path)
     return df['Command'].dropna().tolist()
 
+def log_error_to_file(message, file_name='error_log.txt'):
+    with open(file_name, 'a') as file:
+        file.write(message + '\n')
+
 def connect_and_execute_commands(host, username, password, enable_password, commands):
     device = {
         'device_type': 'cisco_ios',  # Change this depending on your device type
@@ -26,11 +30,17 @@ def connect_and_execute_commands(host, username, password, enable_password, comm
                 output = net_connect.send_command(command)
                 print(f"Output from {host} for command '{command}':\n{output}\n")
     except NetMikoTimeoutException:
-        print(f"Connection timed out for device {host}")
+        error_message = f"Connection timed out for device {host}"
+        print(error_message)
+        log_error_to_file(error_message)
     except NetMikoAuthenticationException:
-        print(f"Authentication failed for device {host}")
+        error_message = f"Authentication failed for device {host}"
+        print(error_message)
+        log_error_to_file(error_message)
     except Exception as e:
-        print(f"An error occurred with device {host}: {e}")
+        error_message = f"An error occurred with device {host}: {e}"
+        print(error_message)
+        log_error_to_file(error_message)
 
 def main():
     # User credentials input
